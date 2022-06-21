ddd17b6884dbd5661814b8d9fd27562188201031
fazliddin-web79 <yakubjonovfazliddin777@gmail.com>
2022-06-21 10:12:09 -0700

    this front

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,3 +1,13 @@
-$(".carousel").carousel({
+document.getElementById(".carousel").carousel({
   interval: 2000,
+});
+
+const oneBtn = document.querySelector("#one-btn");
+const fullBtn = document.querySelector("#full-btn");
+const backModal = document.querySelector("#back-time");
+
+oneBtn.addEventListener("click", () => {
+  console.log("click");
+  console.log("news");
+  backModal.style.display = "none";
 });
